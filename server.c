f931922972834585c36df1c6843f270bcde1cdd2
wowgayman <wow_gayman@163.com>
2018-04-12 12:35:04 +0900

    Update server.c

--- server.c
+++ server.c
@@ -15,33 +15,29 @@ char a[50];
 
 void* runthread(void* arg){
 
-  int listenfd = 0, connfd = 0;
-  int n = 0;
-  struct sockaddr_in serv_addr;
-  char sendBuff[1025];
-  char readBuff[1025];
 
 
-  memset(sendBuff, '0', sizeof(sendBuff));
-  memset(readBuff, '0', sizeof(readBuff));
 
-  serv_addr.sin_family = AF_INET;
-  serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-  serv_addr.sin_port = htons(5000);
   unsigned long i=0;
 
 pthread_t id=pthread_self();
 
 if (pthread_equal(id,tid[0])){
   while (1) {
+    int listenfd = 0, connfd = 0, n = 0;
+    char readBuff[1025];
+    struct sockaddr_in serv_addr;
+    memset(readBuff, '0', sizeof(readBuff));
+    memset(&serv_addr, '0', sizeof(serv_addr));
+    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
     listenfd = socket(AF_INET, SOCK_STREAM, 0);
-  memset(&serv_addr, '0', sizeof(serv_addr));
-  bind(listenfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
-  listen(listenfd, 10);
+    memset(&serv_addr, '0', sizeof(serv_addr));
+    bind(listenfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
+    listen(listenfd, 10);
   while(1){
     connfd = accept(listenfd, (struct sockaddr*)NULL, NULL);
     while ( (n = read(connfd, readBuff, sizeof(readBuff)-1)) > 0)
-        readBuff[n] = 0;
+      readBuff[n] = 0;
     printf("%s\n", readBuff);
     close(connfd);
     sleep(1);
@@ -49,17 +45,19 @@ if (pthread_equal(id,tid[0])){
   }
 }
 else{
+  while(1){
     int sockfd = 0, n = 0;
-    struct sockaddr_in serv_addr;
-    memset(&serv_addr, '0', sizeof(serv_addr));
+    char sendBuff[1025];
+    struct sockaddr_in serv_addr; 
+    memset(sendBuff, '0', sizeof(sendBuff));
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(5000);
-
     inet_pton(AF_INET, a, &serv_addr.sin_addr);
     connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
     scanf("%s",sendBuff);
     write(sockfd,sendBuff,sizeof(sendBuff));
     return 0;
+  }
 }
 
 for (i=0;i<(0xFFFFFFFF);i++);
